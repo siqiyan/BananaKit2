0ca12f35e9dfd8e0769b4e88e63b730bc7663141
siqiyan <yansq2012@gmail.com>
2025-10-27 12:02:40 -0600

    mpu6050 test; gps changed built-in serial

--- gps_imu_module.cpp
+++ gps_imu_module.cpp
@@ -1,6 +1,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <SoftwareSerial.h>
+#include <MPU6050.h>
 
 #include "bananakit.h"
 #include "callstack.h"
@@ -15,8 +16,11 @@
 extern callstack_t Callstack;
 extern bananakit_io_t IO;
 
-SoftwareSerial GPS_Serial(GT_U7_TX, GT_U7_RX);
+// SoftwareSerial GPS_Serial(GT_U7_TX, GT_U7_RX);
 gnss_reader_t *GNSS;
+MPU6050 IMU;
+uint8_t IMU_ok;
+uint8_t Page;
 
 
 static void refresh_display(void);
@@ -27,8 +31,24 @@ void gps_imu_module_init(void) {
     pinMode(GT_U7_RX, OUTPUT);
 
     GNSS = create_gnss_reader();
-    GPS_Serial.begin(9600);
-    GPS_Serial.listen();
+    // GPS_Serial.begin(9600);
+    // GPS_Serial.listen();
+    Serial.begin(9600);
+
+    IMU.initialize();
+    if(IMU.testConnection() == false) {
+        IMU_ok = 0;
+    } else {
+        IMU_ok = 1;
+        IMU.setXAccelOffset(0); //Set your accelerometer offset for axis X
+        IMU.setYAccelOffset(0); //Set your accelerometer offset for axis Y
+        IMU.setZAccelOffset(0); //Set your accelerometer offset for axis Z
+        IMU.setXGyroOffset(0);  //Set your gyro offset for axis X
+        IMU.setYGyroOffset(0);  //Set your gyro offset for axis Y
+        IMU.setZGyroOffset(0);  //Set your gyro offset for axis Z
+    }
+
+    Page = 0;
 
     Serial.begin(9600);
 }
@@ -39,8 +59,21 @@ node_status_t gps_imu_module_update(void) {
     char c;
     uint8_t nmea_start;
     char floatbuf[FLOAT_BUF_SZ];
+    int16_t ax, ay, az;
+    int16_t gx, gy, gz;
 
     // IO.lcd_clear_callback();
+
+    if(!IMU_ok) {
+        snprintf(
+            IO.lcd_buf,
+            LCD_BUF_SIZE,
+            "IMU init fail"
+        );
+        IO.flags = LCD_REFRESH_LINE0;
+        IO.lcd_refresh_callback();
+        return next_status;
+    }
 
     if(GNSS == NULL) {
         snprintf(
@@ -50,8 +83,13 @@ node_status_t gps_imu_module_update(void) {
         );
         IO.flags = LCD_REFRESH_LINE0;
         IO.lcd_refresh_callback();
-        return NODE_EXITING;
+        return next_status;
     }
+
+    IMU.getMotion6(
+        &ax, &ay, &az,
+        &gx, &gy, &gz
+    );
 
     // if(GPS_Serial.available()) {
     //     char c = GPS_Serial.read();
@@ -65,14 +103,14 @@ node_status_t gps_imu_module_update(void) {
     // );
     IO.flags = LCD_REFRESH_LINE0;
     IO.lcd_refresh_callback();
-    while(!GPS_Serial.available()) {
-        delay(50);
-    }
+    // while(!GPS_Serial.available()) {
+    //     delay(50);
+    // }
 
     GNSS->buf_count = 0;
     nmea_start = 0;
-    while(GPS_Serial.available() && GNSS->buf_count < GNSS_UART_BUF_SZ - 1) {
-        c = GPS_Serial.read();
+    while(Serial.available() && GNSS->buf_count < GNSS_UART_BUF_SZ - 1) {
+        c = Serial.read();
         if(c == '$') {
             nmea_start = 1;
         }
@@ -86,52 +124,72 @@ node_status_t gps_imu_module_update(void) {
     IO.lcd_clear_callback();
     gnss_ret = parse_gnss_data_buf(GNSS);
 
-    snprintf(
-        IO.lcd_buf,
-        LCD_BUF_SIZE,
-        "ret:%d d1:%d d2:%d",
-        gnss_ret,
-        GNSS->debug_code,
-        GNSS->debug_code2
-    );
-    IO.flags = LCD_REFRESH_LINE0;
-    IO.lcd_refresh_callback();
-
-    if(GNSS->data_valid) {
-        float2str(GNSS->latitude_minute, floatbuf, FLOAT_BUF_SZ, 5);
+    if(Page == 0) {
         snprintf(
             IO.lcd_buf,
             LCD_BUF_SIZE,
-            "lat:%d %s%c",
-            GNSS->latitude_degree,
-            floatbuf,
-            GNSS->latitude_hemisphere
+            "ret:%d d1:%d d2:%d",
+            gnss_ret,
+            GNSS->debug_code,
+            GNSS->debug_code2
+        );
+        IO.flags = LCD_REFRESH_LINE0;
+        IO.lcd_refresh_callback();
+
+        if(GNSS->data_valid) {
+            float2str(GNSS->latitude_minute, floatbuf, FLOAT_BUF_SZ, 5);
+            snprintf(
+                IO.lcd_buf,
+                LCD_BUF_SIZE,
+                "lat:%d %s%c",
+                GNSS->latitude_degree,
+                floatbuf,
+                GNSS->latitude_hemisphere
+            );
+            IO.flags = LCD_REFRESH_LINE1;
+            IO.lcd_refresh_callback();
+
+            float2str(GNSS->longitude_minute, floatbuf, FLOAT_BUF_SZ, 5);
+            snprintf(
+                IO.lcd_buf,
+                LCD_BUF_SIZE,
+                "lon:%d %s%c",
+                GNSS->longitude_degree,
+                floatbuf,
+                GNSS->longitude_hemisphere
+            );
+            IO.flags = LCD_REFRESH_LINE2;
+            IO.lcd_refresh_callback();
+
+            snprintf(
+                IO.lcd_buf,
+                LCD_BUF_SIZE,
+                "%d %d %d %d",
+                GNSS->utc_hour,
+                GNSS->utc_min,
+                GNSS->year,
+                GNSS->month
+            );
+            IO.flags = LCD_REFRESH_LINE3;
+            IO.lcd_refresh_callback();
+        }
+    } else if(Page == 1) {
+        snprintf(
+            IO.lcd_buf,
+            LCD_BUF_SIZE,
+            "%d %d %d",
+            ax, ay, az
+        );
+        IO.flags = LCD_REFRESH_LINE0;
+        IO.lcd_refresh_callback();
+
+        snprintf(
+            IO.lcd_buf,
+            LCD_BUF_SIZE,
+            "%d %d %d",
+            gx, gy, gz
         );
         IO.flags = LCD_REFRESH_LINE1;
-        IO.lcd_refresh_callback();
-
-        float2str(GNSS->longitude_minute, floatbuf, FLOAT_BUF_SZ, 5);
-        snprintf(
-            IO.lcd_buf,
-            LCD_BUF_SIZE,
-            "lon:%d %s%c",
-            GNSS->longitude_degree,
-            floatbuf,
-            GNSS->longitude_hemisphere
-        );
-        IO.flags = LCD_REFRESH_LINE2;
-        IO.lcd_refresh_callback();
-
-        snprintf(
-            IO.lcd_buf,
-            LCD_BUF_SIZE,
-            "%d %d %d %d",
-            GNSS->utc_hour,
-            GNSS->utc_min,
-            GNSS->year,
-            GNSS->month
-        );
-        IO.flags = LCD_REFRESH_LINE3;
         IO.lcd_refresh_callback();
     }
 
@@ -140,6 +198,14 @@ node_status_t gps_imu_module_update(void) {
     switch(IO.keypress) {
         case PIC_4:
             next_status = NODE_EXITING;
+            break;
+
+        case PIC_0:
+            Page = 0;
+            break;
+
+        case PIC_1:
+            Page = 1;
             break;
 
         default:
@@ -156,7 +222,7 @@ void gps_imu_module_resume(void) {
 
 void gps_imu_module_exit(void) {
     destroy_gnss_reader(GNSS);
-    GPS_Serial.end();
+    Serial.end();
 }
 
 //////////////////////////////////
