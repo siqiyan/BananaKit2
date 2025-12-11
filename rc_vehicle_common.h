d7468398183461fcd09554043b8056b65321049f
siqiyan <yansq2012@gmail.com>
2025-12-10 23:09:22 -0700

    draft update for rc vehicle controller

--- rc_vehicle_common.h
+++ rc_vehicle_common.h
@@ -1,92 +1,101 @@
 // This header file defines shared parameters and data structures for rc vehicle and station.
-// Version: 1.0.0
 
 #ifndef __RC_VEHICLE_COMMON__
 #define __RC_VEHICLE_COMMON__
 
 #include <stdint.h>
 
-#define NRF24_ADDR_BASE "00001"
-#define NRF24_ADDR_VEHICLE "00002"
-#define FRAME_HEADER_ID 0xA0
-#define GPS_F2I_MULTI ((float) 1000000) // convert GPS float to int for communication
-#define CMD_VEL_MULTI ((float) 100)     // convert cmd_vel float to int
-#define MAX_LINEAR_SPEED        0.5     // m/s
-#define MAX_ANGULAR_SPEED       3.14    // rad/s
-#define ADC_R1                  30000.0 // for compute battery voltage
-#define ADC_R2                  7500.0  // for compute battery voltage
-#define ADC_REF_VOLT            5.0
+#define NRF24_ADDR_BASE         "00001"
+#define NRF24_ADDR_VEHICLE      "00002"
+#define FRAME_HEADER_ID         0xA0
+#define GPS_F2I_MULTI           1000000.0   // convert GPS float to int for communication
+#define RAD_F2I_MULTI           1000.0      // convert radian from float to int
+#define CMD_VEL_F2I_MULTI       100.0       // convert cmd_vel float to int
+#define MAX_LINEAR_SPEED        0.5         // m/s
+#define MAX_ANGULAR_SPEED       3.14        // rad/s
 
-typedef struct __status_code__ {
-    uint8_t estop:              1;
-    uint8_t navigate:           1;
-    uint8_t navigate_reply:     1;
-    uint8_t sync_with_laptop:   1;
-    uint8_t sync_with_vehicle:  1;
-} status_code_t;
+// For compute battery voltage:
+// https://lastminuteengineers.com/voltage-sensor-arduino-tutorial/
+#define REF_VOLTAGE             5.0
+#define ADC_R1                  47000.0
+#define ADC_R2                  10000.0
+#define GET_ADC_VOLT(x)         (x * REF_VOLTAGE / 1024.0 * (ADC_R1 + ADC_R2) / ADC_R2)
 
 // (wireless) Vehicle-to-Station dataframe:
 // (nRF24 has built-in checksum verification)
+typedef struct {
+    uint8_t navigate_running:       1;
+    uint8_t gps_data_valid:         1;
+    uint8_t gps_initialized:        1;
+    uint8_t lat_north_positive:     1;
+    uint8_t lon_east_positive:      1;
+} v2s_status_t;
+
 typedef struct __attribute__((__packed__)) __v2s_frame {
-    uint8_t header;
-    status_code_t status_code;
+    v2s_status_t status;
     int64_t timestamp;
     int32_t sequence_id;
-    int32_t latitude_int;
-    int32_t longitude_int;
-    int32_t altitude_int;
-    int32_t orientation_int;
+
+    int16_t lat_degree;
+    int16_t lon_degree;
+    int32_t lat_minute_int;
+    int32_t lon_minute_int;
+    int32_t ekf_x_int;
+    int32_t ekf_y_int;
+    int32_t ekf_yaw_int;
+    int32_t ekf_v_int;
+    int32_t ekf_vyaw_int;
+    uint8_t left_pwm;
+    uint8_t right_pwm;
+
+    int32_t delta_ms;
+    int32_t dist2goal_int;
     int16_t adc_value;
-    uint8_t sm_state;
+    uint8_t debug_code;
 } v2s_frame_t;
 
 // (wireless) Station-to-Vehicle dataframe:
 // (nRF24 has built-in checksum verification)
+typedef struct {
+    uint8_t estop:                  1;
+    uint8_t auto_mode:              1;
+    uint8_t cmd_navigate_start:     1;
+    uint8_t cmd_navigate_cancel:    1;
+} s2v_status_t;
+
 typedef struct __attribute__((__packed__)) __s2v_frame {
-    uint8_t header;
-    status_code_t status_code;
+    s2v_status_t status;
     int64_t timestamp;
     int32_t sequence_id;
-    int16_t twist_x;
-    int16_t twist_y;
-    int16_t twist_yaw;
-    int32_t goal_latitude_int;
-    int32_t goal_longitude_int;
-    int32_t goal_orientation_int;
+    int16_t twist_x_int;
+    int16_t twist_yaw_int;
+    int16_t speed_multi_int;
 } s2v_frame_t;
 
 // (UART) Station-to-laptop dataframe:
-typedef struct __attribute__((__packed__)) __s2l_frame {
-    uint8_t header;
-    status_code_t status_code;
-    int64_t timestamp;
-    int32_t sequence_id;
-    int16_t adc_value;
-    int32_t latitude_int;
-    int32_t longitude_int;
-    int32_t orientation_int;
-    uint8_t sm_state;
-    uint8_t checksum;
-} s2l_frame_t;
+// typedef struct __attribute__((__packed__)) __s2l_frame {
+//     uint8_t header;
+//     packet_status_t status;
+//     int64_t timestamp;
+//     int32_t sequence_id;
+//     int16_t adc_value;
+//     int32_t latitude_int;
+//     int32_t longitude_int;
+//     int32_t orientation_int;
+//     uint8_t sm_state;
+//     uint8_t checksum;
+// } s2l_frame_t;
 
 // (UART) Laptop-to-Station dataframe:
-typedef struct __attribute__((__packed__)) __l2s_frame {
-    uint8_t header;
-    status_code_t status_code;
-    int64_t timestamp;
-    int32_t sequence_id;
-    int32_t goal_latitude_int;
-    int32_t goal_longitude_int;
-    int32_t goal_orientation_int;
-    uint8_t checksum;
-} l2s_frame_t;
-
-
-void init_status_code(status_code_t *code);
-void init_v2s_frame(v2s_frame_t *frame);
-void init_s2v_frame(s2v_frame_t *frame);
-void init_s2l_frame(s2l_frame_t *frame);
-void init_l2s_frame(l2s_frame_t *frame);
-float compute_batt_volt(int16_t adc_value);
+// typedef struct __attribute__((__packed__)) __l2s_frame {
+//     uint8_t header;
+//     packet_status_t status;
+//     int64_t timestamp;
+//     int32_t sequence_id;
+//     int32_t goal_latitude_int;
+//     int32_t goal_longitude_int;
+//     int32_t goal_orientation_int;
+//     uint8_t checksum;
+// } l2s_frame_t;
 
 #endif
